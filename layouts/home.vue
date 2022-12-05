0b8f6a7f5f5135d8f12d3e3a46ec851fc2a6accf
n0xZ <o.gonzalo@hotmail.com>
2022-12-05 20:28:43 -0300

    setup home layout

---
+++ layouts/home.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+	const user = useSupabaseUser()
+	const client = useSupabaseAuthClient()
+
+	const signOut = async () => {
+		await client.auth.signOut()
+	}
+</script>
+
+<template>
+	<div class="h-full w-full">
+		<header class="p-5">
+			<nav
+				class="flex flex-row items-center justify-between container mx-auto max-w-xl"
+			>
+				<h1><NuxtLink to="/home">Nexy</NuxtLink></h1>
+				<ul class="flex flex-row items-center space-x-5">
+					<li><NuxtLink to="/"></NuxtLink></li>
+					<li><NuxtLink to="/"></NuxtLink></li>
+				</ul>
+				<MobileMenu :user="user" :sign-out="signOut" />
+			</nav>
+		</header>
+		<main class="min-h-screen h-full">
+			<slot></slot>
+		</main>
+	</div>
+</template>
