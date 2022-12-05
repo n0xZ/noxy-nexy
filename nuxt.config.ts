d9c578e7bc2f49f8fdb1398f2aa00c4724424d27
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-12-05 17:51:54 -0300

    set unocss custom fonts preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: ['@unocss/nuxt', '@nuxtjs/supabase'],
+	unocss: {
+		webFonts: {
+			provider: 'google',
+			fonts: {
+				inter: 'Inter',
+				roboto: 'Roboto',
+			},
+		},
+	},
 	css: ['@unocss/reset/antfu.css'],
 })
