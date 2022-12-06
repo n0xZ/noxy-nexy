e511e62cec1bd48029f5304d806f16f615336b87
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-12-06 17:43:57 -0300

    replace desktop navbar layout with mobile one

---
+++ components/project/project-list.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type {Project} from '~/types'
+type ProjectListProps={
+   projects:Project[]
+}
+const props = defineProps()
+</script>
+
+<template>
+<article class="grid grid-rows-3 container mx-auto max-w-xl gap-6"></article>
+</template>
