56bcd63f80f5453fa3db93ffc49b43811b17647a
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-12-07 17:40:20 -0300

    setuo initial proejct-list skeleton

--- components/project/project-list.vue
+++ components/project/project-list.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import type {Project} from '~/types'
-type ProjectListProps={
-   projects:Project[]
-}
-const props = defineProps()
+	import type { Project } from '~/types'
+	type ProjectListProps = {
+		projects: Project[]
+	}
+	const props = defineProps<ProjectListProps>()
 </script>
 
 <template>
-<article class="grid grid-rows-3 container mx-auto max-w-xl gap-6"></article>
+	<article class="grid grid-rows-3 container mx-auto max-w-xl gap-6">
+		<aside v-for="project in props.projects">
+			<p>{{ project.title }}</p>
+		</aside>
+	</article>
 </template>
