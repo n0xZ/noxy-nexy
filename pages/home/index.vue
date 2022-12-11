bde5b5b00db9e3fd9b0e1de2c9aaf66d0ccebae8
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-12-11 16:52:33 -0300

    fix issue related to user API call

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,22 +3,28 @@
 	import { Project } from '~~/types'
 
 	useHead({ title: 'Nexy - Home' })
-	const client = useSupabaseClient()
-	const user = useSupabaseUser()
-	const { data, error } = (await client
-		.from('project')
-		.select('*')
-		.eq('userId', user.value?.id)) as PostgrestResponse<Project>
+	const client = useSupabaseClient<Project>()
+
+	const { data: projects } = await useAsyncData('projects', async () => {
+		const loggedUser = await client.auth.getUser()
+		const { data, error } = (await client
+			.from('project')
+			.select('*')
+			.eq('userId', loggedUser.data.user?.id)) as PostgrestResponse<Project>
+		if (error) throw error
+		return data
+	})
 </script>
 
 <template>
-	<section class="mt-16" v-if="!data || !data.length">
+	<section class="mt-16" v-if="!projects || !projects.length">
 		<p>Al parecer no tienes proyectos creados hasta ahora.</p>
 		<NuxtLink class="c-fuchsia-600 underline" to="/home/projects/create"
 			>Haz click acá para empezar a crear tus proyectos</NuxtLink
 		>
 	</section>
 	<section class="h-full w-full" v-else>
-		<ProjectList :projects="data" />
+		<h2 class="text-center text-3xl mb-4">Proyectos actuales</h2>
+		<ProjectList :projects="projects" />
 	</section>
 </template>
