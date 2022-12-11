5d2ded055a9189ca08082c2dd704c3779e65b80d
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-12-11 16:14:46 -0300

    add markdown field  on project type

--- pages/home/projects/create.vue
+++ pages/home/projects/create.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 	import { PostgrestError } from '@supabase/supabase-js'
+	import { marked } from 'marked'
 	import { z } from 'zod'
+
 	import { Project } from '~/types'
 
 	const createProjectSchema = z.object({
 		title: z.string().min(5, { message: 'Campo requerido' }),
 		description: z.string().min(5, { message: 'Campo requerido' }),
+		markdown: z.string(),
 	})
 
 	type CreateProjectFields = z.infer<typeof createProjectSchema>
@@ -19,10 +22,13 @@
 	const projectInputModels = ref<CreateProjectFields>({
 		description: '',
 		title: '',
+		markdown: '',
 	})
 	const errors = ref<Errors>({} as Errors)
 	const isSubmitting = ref(false)
-
+	const parsedMarkdown = computed(() =>
+		marked.parse(projectInputModels.value.markdown)
+	)
 	const onSubmit = async () => {
 		const formResult = createProjectSchema.safeParse(projectInputModels.value)
 		isSubmitting.value = true
@@ -45,13 +51,14 @@
 
 <template>
 	<form
-		class="flex flex-col justify-center space-y-4 container w-full mx-auto max-w-2xl mt-20"
+		class="flex flex-col justify-center space-y-4 xl:p-0 p-3 max-w-2xl"
 		@submit.prevent="onSubmit"
 	>
+		<h2 class="text-center text-2xl font-semibold mb-3">Crear nuevo proyecto</h2>
 		<aside class="flex flex-col justify-center space-y-3 max-w-2xl w-full">
 			<label class="font-bold" for="title">Titulo del proyecto</label>
 			<input
-				class="px-5 py-4 rounded-lg outline-none border-2 border-neutral-100 bg-transparent"
+				class="px-5 py-4 rounded-lg outline-none bg-sky-50"
 				type="text"
 				name="title"
 				placeholder="Jane Doe's first project"
@@ -64,13 +71,13 @@
 			}}</span>
 		</aside>
 
-		<aside class="flex flex-col justify-center space-y-3 max-w-xl w-full">
+		<aside class="flex flex-col justify-center space-y-3 max-w-2xl w-full">
 			<label class="font-bold" for="description">Descripción del proyecto</label>
 			<input
-				class="px-5 py-4 rounded-lg outline-none border-2 border-neutral-100 bg-transparent"
+				class="px-5 py-4 rounded-lg outline-none bg-sky-50"
 				type="text"
 				name="description"
-				placeholder="This is Jane Doe first project. "
+				placeholder="Este es el primer proyecto de Jane Doe. "
 				v-model="projectInputModels.description"
 			/>
 			<span class="c-red-500 h-6">{{
@@ -78,6 +85,28 @@
 					? errors.fieldErrors?.formErrors.fieldErrors.description?.[0]
 					: null
 			}}</span>
+		</aside>
+
+		<aside class="flex flex-col justify-center space-y-3 max-w-2xl w-full">
+			<label class="font-bold" for="description">Contenido del proyecto</label>
+			<textarea
+				class="px-5 py-4 h-64 rounded-lg outline-none bg-sky-50"
+				name="markdown"
+				placeholder="Este contenido también puede ser markdown. "
+				v-model="projectInputModels.markdown"
+			/>
+			<span class="c-red-500 h-6">{{
+				errors.fieldErrors?.formErrors.fieldErrors.markdown
+					? errors.fieldErrors?.formErrors.fieldErrors.markdown?.[0]
+					: null
+			}}</span>
+		</aside>
+		<aside class="flex flex-col justify-center space-y-3 max-w-2xl w-full">
+			<label class="font-medium" for="description">Vista previa del contenido</label>
+			<div
+				class=" min-h-64 h-full w-full rounded-lg outline-none text-base  prose prose-fuchsia"
+				v-html="parsedMarkdown"
+			></div>
 		</aside>
 		<button
 			:class="`px-5 py-3 rounded-lg  w-full max-w-xl ${
