054e9917f85b7bffc1cc3daf653f80ccdd64ff84
n0xZ <o.gonzalo@hotmail.com>
2022-12-12 21:45:09 -0300

    customize unauthorize page

--- pages/home/unauthorized.vue
+++ pages/home/unauthorized.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-	<section><p>No puedes ver este contenido.</p></section>
+	<section class="h-full flex flex-col justify-center items-center space-y-3">
+		<p>No estás autorizado para este contenido.</p>
+		<NuxtLink to="/home" class="underline">Volver atrás</NuxtLink>
+	</section>
 </template>
