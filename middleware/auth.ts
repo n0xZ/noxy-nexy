3fef83aeef47289ba3dfaf9f371455aaa82ec534
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-12-13 11:27:47 -0300

    replace useSupabaseUser composable with useSupabaseAuthClient to perform an async request to get logged user

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,6 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-	const user = useSupabaseUser()
-	if (user) return navigateTo('/home')
+export default defineNuxtRouteMiddleware(async (to, from) => {
+	const client = useSupabaseAuthClient()
+	const loggedUser = await client.auth.getUser()
+	console.log(loggedUser.data.user)
+	if (loggedUser.data.user && !loggedUser.error) return navigateTo('/home')
 })
